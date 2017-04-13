dd83fe86443ea3bb50a6c83ca781f3c7cab9489e
Matt Frawley <docfrawley@Matts-MBP-2.home>
2017-04-13 00:03:48 -0400

    worked on edit and delete functions in hrsadmin

--- public/src/hrsadmin/hrsmem.controller.js
+++ public/src/hrsadmin/hrsmem.controller.js
@@ -42,11 +42,24 @@ function HrsmemController(HrsadminService, list, info, nonlist) {
   }
 
   hmctrl.doEdit = function(index){
-    console.log("doedit: ", index);
+    hmctrl.edItems = hmctrl.meminfo.annual[index];
+    hmctrl.items = [];
+    hmctrl.items.hrstype = hmctrl.edItems.hrstype;
+    if (hmctrl.items.description==null){
+      hmctrl.items.description = hmctrl.edItems.description;
+    }
+    if (hmctrl.items.numhrs==null){
+      hmctrl.items.numhrs = hmctrl.edItems.numhrs;
+    }
+  };
+  hmctrl.madeUpdates = false;
+  hmctrl.makeUpdate = function(){
+    hmctrl.madeUpdates = true;
+    console.log('the result: ', hmctrl.items);
   };
 
   hmctrl.doDelete = function(index){
-    console.log("dodelete: ", index);
+    console.log("dodelete: ", hmctrl.meminfo.annual[index].numid);
   };
 
   hmctrl.optionHPage = function (index){
--- public/src/hrsadmin/memhours.php
+++ public/src/hrsadmin/memhours.php
@@ -78,3 +78,58 @@
       <show-totals
       list="hmctrl.meminfo"></show-totals>
 </div>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Entry for {{hmctrl.edItems.hdate}}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class=row>
+          <div ng-if="hmctrl.madeUpdates" class="alert alert-success" role="alert">
+            Updates have been made in the system</div>
+          <label class="col-sm-2 control-label text-left"
+          for="inputEmail">Type Hrs: </label>
+          <div class="col-sm-4">
+            <select class="custom-select form-control"
+                    ng-model="hmctrl.items.hrstype">
+              <option value="Mercer County">Mercer County</option>
+              <option value="Helpline">Helpline</option>
+              <option ng-show="hmctrl.mgstatus!='A - Trainee'"
+                      value="Continuing Ed">Continuing Ed</option>
+              <option ng-show="hmctrl.mgstatus=='A - Trainee'"
+                      value="Compost (Trainee)">Compost (Trainee)</option>
+            </select>
+         </div>
+
+
+          <label class="col-sm-2 control-label text-right"
+          for="inputEmail"> # of Hrs: </label>
+          <div class="col-sm-4">
+            <input class="form-control" type="text"
+             placeholder={{hmctrl.edItems.numhrs}}
+             ng-model="hmctrl.items.numhrs">
+         </div><br />
+       </div><br />
+       <div class="row">
+          <label class="col-sm-2 control-label text-left"
+          for="inputEmail">Description: </label>
+          <div class="col-sm-10">
+            <input class="form-control" type="text" name="description"
+             placeholder={{hmctrl.edItems.description}}
+             ng-model="hmctrl.items.description">
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" ng-click="hmctrl.makeUpdate()">UPDATE</button>
+      </div>
+    </div>
+  </div>
+</div>
